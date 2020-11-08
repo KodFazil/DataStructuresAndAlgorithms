177b5c624e58ee2d2e6c28d8a2a1bcd3676a8464
KodFazil <57687767+KodFazil@users.noreply.github.com>
2020-11-08 21:55:41 +0300

    Add files via upload

--- Strings/CountingWords&VowelsinaString.c
+++ Strings/CountingWords&VowelsinaString.c
@@ -4,7 +4,7 @@ void countVowels(char a[]) {
 	int i, vowelCount=0, consonantCount=0;
 	for (i=0; a[i]!='\0'; i++) {
 		if (a[i]=='a' || a[i]=='e' || a[i]=='i' || a[i]=='u' || a[i]=='o' || 
-	        a[i]=='A' || a[i]=='E' || a[i]=='I' || a[i]=='U' || a[i]=='O') {
+	            a[i]=='A' || a[i]=='E' || a[i]=='I' || a[i]=='U' || a[i]=='O') {
 		    	vowelCount++;
 			}
 		else if ((a[i]>=65 && a[i]<=97) || (a[i]>=97 && a[i]<=122)) {
